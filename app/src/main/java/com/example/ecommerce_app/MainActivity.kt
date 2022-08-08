785111c34a55e47b15d4b9193e77406023d0d9d3
DELL <Ayaabed22>
2022-08-08 07:10:28 -0700

    Create Disgn Signin

--- app/src/main/java/com/example/ecommerce_app/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/MainActivity.kt
@@ -1,43 +1,38 @@
  package com.example.ecommerce_app
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ecommerce_app.ui.theme.ECommerceAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ECommerceAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+ import android.os.Bundle
+ import androidx.activity.ComponentActivity
+ import androidx.activity.compose.setContent
+ import androidx.compose.runtime.Composable
+ import androidx.compose.runtime.rememberCompositionContext
+ import androidx.navigation.NavHost
+ import androidx.navigation.compose.NavHost
+ import androidx.navigation.compose.composable
+ import androidx.navigation.compose.rememberNavController
+ import com.example.ecommerce_app.ui.theme.ECommerceAppTheme
+ import com.example.ecommerce_app.ui.theme.LoginPage
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ECommerceAppTheme {
-        Greeting("Android")
-    }
-}
+ class MainActivity : ComponentActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContent {
+             ECommerceAppTheme {
+                 navigatepage()
+             }
+         }
+     }@Composable
+     fun navigatepage(){
+         val navController= rememberNavController()
+         NavHost(navController = navController, startDestination = "Login_Page",
+             builder = {
+                 composable("Login_Page", content = { LoginPage(navController = navController)})
+             }
+         )
+     }
+ }
+
+
+
+
